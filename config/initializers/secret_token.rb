0b8f534d460cbe8be0a1497a58eb59f279601856
darm96 <diego.ruiz@civico.com>
2016-11-09 11:45:15 -0500

    add mvc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiviDate::Application.config.secret_token = '856eff184b4d0efe204226035bcace88f8f127bafb549a2b4d1f87dd96013d6f6521a16e930aca899257afc03b685f86c9fb2caae459789ce80001c2faa05668'
